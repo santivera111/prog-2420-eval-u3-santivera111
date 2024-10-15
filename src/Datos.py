8a5418bb713e106158301aca475939ec2fbc6117
santivera111 <santivera111@gmail.com>
2024-10-14 20:27:03 -0500

    ACTUALIZACION Y EXPLICACION
    Realizo una actualización con una pequeña explicación del paso a paso, para así facilitarme el paso a paso que debo de seguir y recordar en cada momento que siga con el proyecto

--- src/Datos.py
+++ src/Datos.py
@@ -1,9 +1,10 @@
+ #TRABAJO 3
 
 def VShore():
       pass
 
 
-Iphone13 = {}
+ #La idea principal del codigo es realizar una tienda web donde se puedan realizar varias compras de celulares, para eso estoy haciendo un directorio con descripcion de los celulares
 
 
 Iphones = {
@@ -14,8 +15,8 @@ Iphones = {
     "Memoria": "128 GB",
     "Color": ["Rosa","Verde","Negro"],
     "Memoria RAM": " 4GB",
-    "Tamaño": "Pulgadas",
-    "Peso": "173 g"
+    "Tamaño": "5.2 Pulgadas",
+    "Peso": "160 g"
 
     }
 
@@ -23,8 +24,13 @@ Samsumgs = {
     "Nombre": "Samsumg_Galaxy_S23",
     "Precio": "3.800.000",
     "Envio": 5.000,
-    "Descripcion": "512 GB color verde"
-#Se debe de realizar todo el paso a paso del anterior diccionario, igual para el xiamoi
+    "Descripcion": " GB color verde"
+        "Memoria": "512 GB",
+    "Color": ["Azul","Rojo","Negro"],
+    "Memoria RAM": " 4GB",
+    "Tamaño": "4.5 Pulgadas",
+    "Peso": "155 g"
+
     }
 
 Xiaomi = {
@@ -32,7 +38,94 @@ Xiaomi = {
     "Precio": "1.500.000",
     "Envio": 5.000,
     "Descripcion": "512GB color Negro"
+        "Memoria": "128 GB",
+    "Color": ["Gris","Blanco","Negro"],
+    "Memoria RAM": " 4GB",
+    "Tamaño": "5.0 Pulgadas",
+    "Peso": "157 g"
     }
+Xiaomi_X6_Pro = {
+    "Nombre": "Xiaomi_X6_Pro",
+    "Precio": "1.500.000",
+    "Envio": 5.000,
+    "Descripcion": ""
+        "Memoria": "512 GB",
+    "Color": ["Gris","Blanco","Negro"],
+    "Memoria RAM": " 12GB",
+    "Tamaño": "6.67 Pulgadas",
+    "Peso": "157 g"
+    }
+Moto = {
+    "Nombre": "Moto_G24",
+    "Precio": "1.500.000",
+    "Envio": 5.000,
+    "Descripcion": "512GB color Negro"
+        "Memoria": "128 GB",
+    "Color": ["Gris","Blanco","Negro"],
+    "Memoria RAM": " 4GB",
+    "Tamaño": "5.0 Pulgadas",
+    "Peso": "157 g"
+    }
+
+
+
+
+
+
+
+#Despues de realizar el diccionario de cada uno debo de realizar una matriz donde esten cada uno de estos, y en cualquier momento llamar cualquier descripcion de estos para compararlas
+
+def Menu1():   
+    print("1. Elegista la opcion Costo")
+    print("Elegista la opcion Costo de Envio")
+    print("Elegista la opcion Marca")
+    opcion = int(input("Ingrese opcion"))
+
+
+    Eleccion = input("Ingrese el numero del menu que desea ver n11.Costo\n2 2.Costo de envio\n3 3.Marca")
+    if Eleccion == 1:
+        print(opcion1)
+    elif Eleccion == 2:
+        print(opcion2)
+    else:
+        Eleccion== 3
+        print(opcion3)
+    
+
+#Posterior a esto debo de poder comparar las descripciones de cada uno, por medio de una lista de descripciones
+    
+
+
+
+
+
+
+
+
+
+
+#FInalmente realizar un menu con cada una de los celulares disponibles, para poder seleccionarlos, generando otra lista
+
+
+
+
+
+#Otra opcion es realizar un carrito donde se use la funcion del sum, para sumar los productos del carrito
+
+
+    Costo = ("Iphone", "Samsung", "Xiaomi")
+    CostoEnvio = ("Samsung","Xiaomi","Iphone")
+    Marca = ("Iphone","Xiaomi","Samsung")
+
+if __name__ == "Menu1":
+    
+    
+
+
+
+
+if __name__ == "__VShore__":
+    VShore()
 
 
 
